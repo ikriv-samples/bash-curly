eb8bac033beb4ba51ac8c1a68d376e94d95f33f2
Ivan Krivyakov <ivan@ikriv.com>
2020-11-23 17:40:38 -0500

    Added ability to test output with repeating sequences

--- tests.py
+++ tests.py
@@ -11,27 +11,29 @@ class TestProcessExpression(unittest.TestCase):
 
     def test_parser(self):
         test_data = {
-            "": {""},
-            "abc": {"abc"},
-            "{foo,bar}": {"foo", "bar"},
-            "a{b,c}d": {"abd", "acd"},
-            "{a,b}{c,d}": {"ac", "bc", "ad", "bd"},
-            "a{b,{c,d}e}": {"ab", "ace", "ade"},
-            "{a,b}{{c,d}{e,f}}": {"ace", "acf", "ade", "adf", "bce", "bcf", "bde", "bdf"},
+            "": [""],
+            "abc": ["abc"],
+            "{foo,bar}": ["foo", "bar"],
+            "a{b,c}d": ["abd", "acd"],
+            "{a,b}{c,d}": ["ac", "bc", "ad", "bd"],
+            "a{b,{c,d}e}": ["ab", "ace", "ade"],
+            "{a,b}{{c,d}{e,f}}": ["ace", "acf", "ade", "adf", "bce", "bcf", "bde", "bdf"],
 
-            "{a,b,c}": {"a","b","c"},
-            "a{b,c}": {"ab", "ac"},
-            "{a}": {"a"},
-            "a{b,c}d{e,f,g}": {"abde", "abdf", "abdg", "acde", "acdf", "acdg"},
-            "a{b{xX{u,VvV},yY},c}":  {"abxXu", "abxXVvV", "abyY", "ac"},
-            "z{a,b,c{d,e}}": {"za","zb", "zcd", "zce"},
+            "{a,b,c}": ["a","b","c"],
+            "a{b,c}": ["ab", "ac"],
+            "{a}": ["a"],
+            "a{b,c}d{e,f,g}": ["abde", "abdf", "abdg", "acde", "acdf", "acdg"],
+            "a{b{xX{u,VvV},yY},c}":  ["abxXu", "abxXVvV", "abyY", "ac"],
+            "z{a,b,c{d,e}}": ["za","zb", "zcd", "zce"],
             "}abc": Exception,
             "{abc": Exception,
-            "}{": Exception,
-            "{}": {""},
+            "}[": Exception,
+            "{}": [""],
             "a,b,c": Exception,
             "{a{b,c}": Exception,
-            "{a,}": {"", "a"}
+            "{a,}": ["", "a"],
+            "{a,a}": ["a", "a"],
+            "{a,ab}{c,bc}": ["ac", "abc", "abc", "abbc"]
         }
         for (expression, expected_result) in test_data.items():
             with self.subTest(expression=expression):
@@ -40,9 +42,7 @@ class TestProcessExpression(unittest.TestCase):
                     result = [*process_expression(expression)]
 
                 if expected_result != Exception:
-                    result_set = set(result)
-                    self.assertEqual(len(result), len(result_set))
-                    self.assertEqual(result_set, expected_result)
+                    self.assertEqual(sorted(result), sorted(expected_result))
 
 if __name__ == "__main__":
   unittest.main()
